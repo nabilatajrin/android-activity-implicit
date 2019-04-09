792e9eede62c67a80cac8058453507c6db6cf0f8
Nabila <nabilatajrin@gmail.com>
2019-04-09 09:08:17 +0600

    09:07PM 09.MAR.2019 first upload

---
+++ app/src/main/java/bd/org/rain/activityimplicit/MainActivity.java
@@ -0,0 +1,19 @@
+package bd.org.rain.activityimplicit;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.ImageView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        ImageView imageView = findViewById(R.id.img_one);
+        imageView.setImageURI((Uri) getIntent().getExtras().get(Intent.EXTRA_STREAM));
+    }
+}
