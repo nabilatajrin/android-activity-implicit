e123ef3949c682617656f6bd4ffac2342a3dd2a3
Nabila Tajrin <nabilatajrin@users.noreply.github.com>
2020-12-28 12:16:34 +0600

    Update MainActivity.java

--- app/src/main/java/bd/org/rain/activityimplicit/MainActivity.java
+++ app/src/main/java/bd/org/rain/activityimplicit/MainActivity.java
@@ -7,12 +7,10 @@ import android.os.Bundle;
 import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
         ImageView imageView = findViewById(R.id.img_one);
         imageView.setImageURI((Uri) getIntent().getExtras().get(Intent.EXTRA_STREAM));
     }
